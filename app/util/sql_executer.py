fc76bad649dfef243ad6c38cc4cbcad9652e00fe
cogofoward <hhgoon@naver.com>
2021-12-26 23:42:51 +0900

    DELETE 쌍따옴표 만드는 함수

--- app/util/sql_executer.py
+++ app/util/sql_executer.py
@@ -21,27 +21,25 @@ class SqlExecuterImpl(SqlExecuter):
         )
         self.cursor = self.__db.cursor(DictCursor)
 
-    def insert_bot(self,bot_id: str, psid: str):
-        bot_id = self.add_double_quotation_mark(bot_id)
-        psid = self.add_double_quotation_mark(psid)
+    def insert_bot(self, bot_id: str, psid: str):
         uuid = "unhex(replace(uuid(),'-',''))"
         self.cursor.execute(
-            f'insert into bot(id,name,created_at,psid) values({uuid},{bot_id},now(),{psid})'
+            f'insert into bot(id,name,created_at,psid) values({uuid},"{bot_id}",now(),"{psid}")'
         )
         self.__db.commit()
 
 
-    def insert_bot_repo_id(self): pass
+    def update_bot_repo_id(self, repo_id: str, bot_name: str, psid: str):
+        psid = self.add_double_quotation_mark(psid)
+        self.cursor.execute(
+            f'UPDATE bot set repo_id="{repo_id}", where name="{bot_name}" and  psid="{psid}"'
+        )
 
     def insert_repository(self): pass
 
     def delete_bot_from_repo(self): pass
 
     def delete_repo_from_bot(self): pass
-
-    @staticmethod
-    def add_double_quotation_mark(param: str):
-        return '"'+param+'"'
 
 if __name__ == '__main__':
     a = SqlExecuterImpl(
