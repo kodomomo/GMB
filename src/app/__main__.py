6f57a3c16187292c48cebcbcb43952b0ac542c60
cogofoward <hhgoon@naver.com>
2022-03-20 03:34:09 +0900

    ♻️ :: update 줄바꿈

--- src/app/__main__.py
+++ src/app/__main__.py
@@ -1,4 +1,3 @@
 from src.app import create_app
 
 app = create_app()
-
--- src/app/util/dao/bot.py
+++ src/app/util/dao/bot.py
@@ -5,13 +5,14 @@ from datetime import datetime
 
 from ..dao import Base
 
+
 class BotModel(Base):
     __tablename__ = 'bot'
 
     bot_id = Column('bot_id', VARCHAR(250), primary_key=True)
-    repo_name = Column('repo_name', VARCHAR(150), ForeignKey('repository.repo_name'),nullable=True)
-    created_at = Column('created_at',DATETIME,nullable=False)
+    repo_name = Column('repo_name', VARCHAR(150), ForeignKey('repository.repo_name'), nullable=True)
+    created_at = Column('created_at', DATETIME, nullable=False)
 
     def __init__(self):
         self.bot_id = uuid4()
-        self.created_at  =datetime.now().replace(microsecond=0)
+        self.created_at = datetime.now().replace(microsecond=0)
