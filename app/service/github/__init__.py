a4cdfa8fe1f15929ea4bdb9ecf2167b59b16a88e
cogofoward <hhgoon@naver.com>
2022-03-31 22:24:00 +0900

    fix :: typing error

--- app/service/github/__init__.py
+++ app/service/github/__init__.py
@@ -3,6 +3,6 @@ from .github_service import GithubService
 act_service_by_type = {
     'ping': GithubService.when_ping,
     'issues': GithubService.when_issue,
-    'pull_requests': GithubService.when_pr
+    'pull_request': GithubService.when_pr
 }
 
