37ba893a8264f5586d08356f37fbdfb526572b79
cogofoward <hhgoon@naver.com>
2022-04-01 14:53:10 +0900

    fix :: write return type

--- app/service/github/github_service_impl.py
+++ app/service/github/github_service_impl.py
@@ -4,17 +4,17 @@ from ...util.parser.github_parser import GithubParser
 class GithubServiceImpl:
 
     @staticmethod
-    def when_ping(bot_id: str, body: dict):
+    def when_ping(bot_id: str, body: dict) -> dict:
         print(bot_id)
         repo_name = GithubParser.get_repository_name(body)
         return repo_name
 
     @staticmethod
-    def when_issue(bot_id: str, body: dict):
+    def when_issue(bot_id: str, body: dict) -> dict:
         print(bot_id)
         return GithubParser.parse_issues(body)
 
     @staticmethod
-    def when_pr(bot_id: str, body: dict):
+    def when_pr(bot_id: str, body: dict) -> dict:
         print(bot_id)
         return GithubParser.parse_pr(body)
