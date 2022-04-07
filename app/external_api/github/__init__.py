a2160fe74d152d88ad270df7a75d43025c8d0141
cogofoward <hhgoon@naver.com>
2022-04-07 10:37:57 +0900

    fix :: not get

--- app/external_api/github/__init__.py
+++ app/external_api/github/__init__.py
@@ -9,12 +9,11 @@ github_router = APIRouter(
 
 @github_router.post('/{bot_id}')
 async def get_webhook_by_each_bot(bot_id: str, request: Request):
-
     try:
         type = request.headers.get('X-GitHub-Event')
         body = await request.json()
 
-        return act_service_by_type.get(type)(bot_id, body)
+        return act_service_by_type[type](bot_id, body)
 
     except KeyError:
         return 'None Provide Type'
