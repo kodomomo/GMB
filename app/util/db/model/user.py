6e4a090cb5a33d8066a44c610f8d90715274f408
cogofoward <hhgoon@naver.com>
2022-04-11 02:03:51 +0900

    add :: user

---
+++ app/util/db/model/user.py
@@ -0,0 +1,21 @@
+from ..model import Base
+
+from sqlalchemy import Column, VARCHAR, DATETIME, ForeignKey
+
+from uuid import uuid4
+
+from datetime import datetime
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    psid = Column(VARCHAR(250), primary_key=True)
+    bot_id = Column(VARCHAR(250), nullable=False, default=uuid4())
+    repo_name = Column(VARCHAR(150), ForeignKey('repository.repo_name'), nullable=True)
+    registered_at = Column(DATETIME, nullable=False)
+
+    def __init__(self, psid: str):
+        self.psid = psid
+        self.bot_id = uuid4()
+        self.registered_at = datetime.now().replace(microsecond=0)
