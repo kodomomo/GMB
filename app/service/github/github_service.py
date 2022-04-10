8e009911439a10f4651bd213fccb8de8cbf1cf13
cogofoward <hhgoon@naver.com>
2022-04-11 02:04:19 +0900

    update :: github db done

--- app/service/github/github_service.py
+++ app/service/github/github_service.py
@@ -1,24 +1,26 @@
-from ...util.db.decorater.db_decorater import create_new_bot, update_event_amount
+from ...util.db.decorater.db_decorater import create_bot,assign_bot, increase_event_amount
 
 
 class GithubService:
     """
     데코리어터를 사용한 이유
             1 : 함수 내부 로직을 보이지 않고 사용할 수 있음 -> githubservice class를 인터페이스 처럼 사용가능 함
-            2 : githubservice에 함수의 행동이 아니라 함수 호출 경우를 이름으로 명시할 수 있게함
-            3 : 함수는 한 번에 한 가지 역할을 해야하지만, 데코레이터를 통하면 여러가지 일을 진행할 수 있음.
-                -> when 함수가 실행하는 게  아니라 데코레이터 함수가 실행시키는 거니까
-            4 : 코드가 간결해짐
+            2 : 데코레이터를 통해 부가적인 일을 수행함으로 함수가 한 가지의 로직을 하는 데 집중할 수 있게 해 줌
+            3 : 코드가 간결해짐
     """
 
     @staticmethod
-    @create_new_bot
-    def when_ping(bot_id: str, type: str, body: dict) -> dict: return body
+    @create_bot
+    @assign_bot
+    def when_ping(bot_id: str, type: str, body: dict) -> dict:
+        return body
 
     @staticmethod
-    @update_event_amount
+    @assign_bot
+    @increase_event_amount
     def when_issue(bot_id: str, type: str, body: dict) -> dict: return body
 
     @staticmethod
-    @update_event_amount
+    @assign_bot
+    @increase_event_amount
     def when_pr(bot_id: str, type: str, body: dict) -> dict: return body
--- app/util/db/decorater/db_decorater.py
+++ app/util/db/decorater/db_decorater.py
@@ -2,44 +2,58 @@ from ...db import get_session
 
 from ...parser import parsed_by_type
 
+from ..model.user import User
 from ..model.repository import Repository
 
 
-# 데코레이터마다 세션을 생성하는 것은 그릇된 행동이 아닐까?
-
-def create_new_bot(func):
-    def wrapper(bot_id: str, type: str, body: dict):
-        session = get_session()
-        repo_name = parsed_by_type[type](body)
-
-        try:
-            session.add(Repository(repo_name['name']))
-            session.commit()
-        except:
-            session.rollback()
-
-        return func(bot_id, type, repo_name)
-
-    return wrapper
-
-
-def update_event_amount(func):
+def create_bot(func):
     session = get_session()
 
     def wrapper(bot_id: str, type: str, body: dict):
-        parsed_body = parsed_by_type[type](body)
+        name = parsed_by_type[type](body)['repo_name']
+        new_repo = Repository(name)
         try:
-            session.query(Repository).filter(Repository.repo_name == parsed_body['repo_name']).update(
-                {'event_amt': Repository.event_amt + 1}
-            )
+            session.add(new_repo)
             session.commit()
         except:
             session.rollback()
-
-        return func(bot_id, type, parsed_body)
+        return func(bot_id, type, body)
 
     return wrapper
 
-# notice_by_messenger
 
-# add_repository_to_each_user
+def assign_bot(func):
+    session = get_session()
+
+    def wrapper(bot_id: str, type: str, body: dict):
+        name = parsed_by_type[type](body)['repo_name']
+        try:
+            session.query(User).filter(User.bot_id == bot_id).update(
+                {'repo_name': name}
+            )
+            session.commit()
+
+        except: session.rollback()
+
+        return func(bot_id, type, body)
+
+    return wrapper
+
+
+def increase_event_amount(func):
+    session = get_session()
+
+    def wrapper(bot_id: str, type: str, body: dict):
+        name = parsed_by_type[type](body)['repo_name']
+        try:
+            session.query(Repository).filter(Repository.repo_name==name).update(
+                {'event_amt': Repository.event_amt + 1}
+            )
+            session.commit()
+
+        except: session.rollback()
+
+        return func(bot_id, type, body)
+
+    return wrapper
+# send_messenger(func):
