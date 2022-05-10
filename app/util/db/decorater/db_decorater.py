0e8258009a386e9882c6a6404ba0123c50305423
WilterFlip <hhgnoon@naver.com>
2022-05-10 19:58:34 +0900

    add :: select all repository

--- app/util/db/decorater/db_decorater.py
+++ app/util/db/decorater/db_decorater.py
@@ -16,9 +16,11 @@ def create_bot(func):
             session.add(new_repo)
             session.commit()
 
-        except: session.rollback()
+        except:
+            session.rollback()
 
-        finally: close_session(session)
+        finally:
+            close_session(session)
 
         return func(bot_id, type, body)
 
@@ -61,5 +63,23 @@ def increase_event_amount(func):
         finally: close_session(session)
 
         return func(bot_id, type, body)
+
+    return wrapper
+
+
+def select_all_repository_by_event_amt(func):
+
+    session = get_session()
+
+    def wrapper():
+
+        try:
+            a = session.query(Repository).order_by(Repository.event_amt.desc())
+
+            return [i for i in a]
+
+        except: session.rollback()
+
+        finally: close_session(session)
 
     return wrapper
