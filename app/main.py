8b02682fb21942ffca647a0d817d9c0d15ddb9b2
WilterFlip <hhgnoon@naver.com>
2022-05-21 16:25:36 +0900

    update :: show_rank

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from app import create_app
 
-from .service.our import our_provide_service
+from app.service.our import our_service
+
+from fastapi.requests import Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = create_app()
+templates = Jinja2Templates(directory="/Volumes/Tools/Project/Python/Kodomo-Dragon/app/templates")
 
-@app.get('/')
-def show_repository_lank(): return our_provide_service['show_rank']()
+
+@app.get('/', response_class=HTMLResponse)
+def show_repository_lank(request: Request):
+    return templates.TemplateResponse('table.html', context={
+        'request': request,
+        'repo_list': our_service['show_rank']()
+    })
