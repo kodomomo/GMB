e9d9da89001b13d5779cec09881e91c4f50bd2ba
osangu <osangu012@gmail.com>
2023-02-06 16:58:23 +0900

    [UDPATE] github router 분리

--- app/core/github/__init__.py
+++ app/core/github/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
-from app.core.github.api import github_router
+from app.core.github.api.webhook import github_webhook_router
+from app.core.github.api.oauth import github_oauth_router
 
 
 def include_github_router(app: FastAPI):
-    app.include_router(github_router)
+    app.include_router(github_webhook_router)
+    app.include_router(github_webhook_router)
---
+++ app/core/github/api/oauth.py
@@ -0,0 +1,6 @@
+from fastapi import APIRouter, Request
+
+github_oauth_router = APIRouter(
+    prefix='/github/oauth'
+)
+
