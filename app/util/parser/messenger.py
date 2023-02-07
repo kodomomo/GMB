b1619f1dd411822756e905b7d11dae71dd222c8b
osangu <osangu012@gmail.com>
2023-02-07 21:35:13 +0900

    [ADD]: messenger parser

---
+++ app/util/parser/messenger.py
@@ -0,0 +1,13 @@
+from app.core.messenger.api import MessagePayload
+from app.util.time import timestamp_to_datetime
+
+
+def message_payload_to_message(payload: MessagePayload):
+    entry = payload.entry[0]['messaging'][0]
+
+    return {
+        'content': entry['message'].get('text'),
+        'sender': entry['sender']['id'],
+        'receipt': entry['recipient']['id'],
+        'send_at': timestamp_to_datetime(entry['timestamp'])
+    }
