ceb0ba197dfbececbcf1796bfe367a9d0bd4672e
osangu <osangu012@gmail.com>
2023-02-08 19:13:26 +0900

    [ADD] messenger config

--- app/config/messenger/__init__.py
+++ app/config/messenger/__init__.py
@@ -1,1 +1,10 @@
-MESSENGER_WEBHOOK_VERIFY_TOKEN = 'qwer1234'
+from os import environ
+
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+APP_ID = environ['MESSENGER_APP_ID']
+APP_SECRET = environ['MESSENGER_APP_SECRET']
+
+MESSENGER_WEBHOOK_VERIFY_TOKEN = environ['MESSENGER_WEBHOOK_VERIFY_TOKEN']
