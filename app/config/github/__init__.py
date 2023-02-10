f6c489113c871e13fae92b03167ac05c8f7ea5bd
osangu <osangu012@gmail.com>
2023-02-10 16:12:49 +0900

    [UPDATE]  Config 값 변경

---
+++ app/config/github/__init__.py
@@ -0,0 +1,5 @@
+from os import environ
+
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv(filename='.env'))
+
--- app/config/messenger/__init__.py
+++ app/config/messenger/__init__.py
@@ -1,5 +1,4 @@
 from os import environ
-
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
@@ -8,3 +7,6 @@ APP_ID = environ['MESSENGER_APP_ID']
 APP_SECRET = environ['MESSENGER_APP_SECRET']
 
 MESSENGER_WEBHOOK_VERIFY_TOKEN = environ['MESSENGER_WEBHOOK_VERIFY_TOKEN']
+REDIRECT_URI = 'https://ac1a-168-188-240-208.jp.ngrok.io/facebook/redierect'
+STATE = environ['STATE']
+PAGE_ID = environ['PAGE_ID']
