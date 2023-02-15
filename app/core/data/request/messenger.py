ff6bff2a1167ea3c958e6b89440450b68b792a19
osangu <osangu012@gmail.com>
2023-02-16 01:07:56 +0900

    [UPDATE] raw string

--- app/core/data/request/messenger.py
+++ app/core/data/request/messenger.py
@@ -21,10 +21,12 @@ def send_message(
         headers={"Content-Type": "application/json"},
     )
 
-    
-PENDING_MESSAGE = '1️⃣ 해당 Repository > Settings > Webhooks > Add Webhoook\n' \
-                  '2️⃣ Payload URL : https://3e08-168-188-234-72.jp.ngrok.io/{webhook_id}\n' \
-                  '3️⃣ Content type : application/json \n' \
-                  '4️⃣ Secret : {secret}\n\n' \
-                  '⚠️ 보안을 위해 Secret을 설정해주세요.\n' \
-                  '⛔️ Content type != json 일 경우 작동하지 않습니다.'
+
+PENDING_MESSAGE = r"ℹ️ HOW TO USE\n\n\n" \
+                  r"1️⃣ 해당 Repository > Settings > Webhooks > Add Webhook\n\n" \
+                  r"2️⃣ Payload URL : https://20d5-168-188-234-72.ngrok.io/{webhook_id} \n\n" \
+                  r"3️⃣ Content type : application/json \n\n" \
+                  r"4️⃣ Secret : {secret} \n\n\n" \
+                  r"=========================\n\n" \
+                  r"⛔️ 보안을 위해 Secret을 설정해주세요. ⛔️\n" \
+                  r"⛔️ Content type != json 일 경우 작동하지 않습니다. ⛔️"
