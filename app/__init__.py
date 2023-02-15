7f99d6760206a390509252d30996716b2f088872
osangu <osangu012@gmail.com>
2023-02-15 16:52:52 +0900

    [ADD] mongoDB initializer

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 
+from app.core.data.mongo import init_mongo
 from app.core.github import include_github_router
-from app.core.messenger import include_messenger_router
+from app.core.facebook import include_messenger_router
 
 
 def create_app():
     app = FastAPI()
 
+    # Mongo
+    init_mongo()
+
+    # Router
     include_github_router(app)
     include_messenger_router(app)
 
---
+++ app/core/data/mongo/__init__.py
@@ -0,0 +1,58 @@
+from typing import List
+
+from pymongo import MongoClient, ASCENDING, IndexModel
+from pymongo.database import Database
+from pymongo.errors import CollectionInvalid
+# from pymongo.typings import _DocumentType as DocumentType
+
+from app.common.exception import throw
+from app.common.exception.mongodb import DatabaseNotFoundException
+
+from app.config.mongo import MONGO_HOST, MONGO_PORT, MONGO_DATABASE
+from app.core.data.mongo.collections import CollectionNames, PendingWebhook
+
+
+def get_mongo_db(host: str, port: int, database: str) -> Database:
+    mongodb = MongoClient(
+        host=host,
+        port=port,
+        document_class=dict,
+        tz_aware=False
+    ).get_database(database)
+
+    # 익셉션 처리가 아니라, 새로운 db에서 작동하게 해야할까? TODO
+
+    return mongodb if len(mongodb.list_collection_names()) != 0 else throw(DatabaseNotFoundException)
+
+
+def initialize_collections(db: Database, collection_names: List[str]):
+    for name in collection_names:
+
+        try:
+            db.create_collection(name)
+
+        except CollectionInvalid:
+            pass
+
+
+def set_ttl(db: Database, collection_name: str, document_name: str, ttl_sec: int):
+    create_index(
+        db, collection_name, document_name, ttl_sec=ttl_sec
+    )
+
+
+def create_index(db: Database, collection_name: str, document_name: str, ttl_sec: int):  # TODO
+    collection = db.get_collection(collection_name)
+    index = IndexModel([(document_name, ASCENDING)], expireAfterSeconds=ttl_sec)
+
+    collection.create_indexes([index])
+
+
+def init_mongo():
+    initialize_collections(mongo, CollectionNames.NAME_LIST)
+    set_ttl(mongo, CollectionNames.PENDING_WEBHOOK, PendingWebhook.CREATED_AT, 1)
+
+
+mongo = get_mongo_db(
+    MONGO_HOST, MONGO_PORT, MONGO_DATABASE
+)
