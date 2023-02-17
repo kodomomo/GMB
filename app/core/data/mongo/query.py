03f41b84dabe403e30d659c5c69474fb508a45a6
osangu <osangu012@gmail.com>
2023-02-17 14:37:29 +0900

    [ADD] get pending webhook

--- app/core/data/mongo/query.py
+++ app/core/data/mongo/query.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from app.core.data.mongo import get_collection
 from app.core.data.mongo.collections import CollectionNames
 
@@ -6,3 +8,10 @@ def get_page_access_token():
     collection = get_collection(CollectionNames.TOKEN)
 
     return collection.find_one()['_id']
+
+
+def get_pending_webhook(id_: UUID):
+    collection = get_collection(CollectionNames.PENDING_WEBHOOK) # TODO
+    return collection.find(
+        {'_id': str(id_)},
+    )
