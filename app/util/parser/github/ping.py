7974bad76535b9fe12bd531dca8908c3870bef13
osangu <osangu012@gmail.com>
2023-02-18 17:14:38 +0900

    [UPDATE] html_url

--- app/util/parser/github/ping.py
+++ app/util/parser/github/ping.py
@@ -4,5 +4,5 @@ def parse_ping_event(payload: dict):
         'github_name': payload['sender']['login'],
         'repository_id': payload['repository']['id'],
         'repository_name': payload['repository']['name'],
-        'repository_url': payload['repository']['url'],
+        'repository_url': payload['repository']['html_url'], #TODO
     }
