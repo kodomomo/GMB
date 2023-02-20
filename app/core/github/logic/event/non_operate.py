e396bebd963ea64476668ba01ac4adfb95989f19
osangu <osangu012@gmail.com>
2023-02-20 13:50:13 +0900

    [UPDATE] 매개변수 정정

--- app/core/github/logic/event/non_operate.py
+++ app/core/github/logic/event/non_operate.py
@@ -7,7 +7,7 @@ from app.core.data.mongo.webhook.query import get_webhook_by_id
 from app.core.data.request.messenger import send_message
 
 
-async def handle_none_provide_event(bot_id: UUID, event_type: str, request: Request):
+async def handle_none_provide_event(bot_id: UUID, event_type: str, request: dict):
     webhook = get_webhook_by_id(bot_id)
 
     message = webhook[Webhook.REPOSITORY][Repository.NAME] + rf' Repository에서 {event_type} Event가 발생하였습니다.\n\n' \
