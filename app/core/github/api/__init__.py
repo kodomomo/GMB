b23c0330d8d177c59e3201c20f36982452f7892b
osangu <osangu012@gmail.com>
2023-02-20 17:13:31 +0900

    [UPDATE] 튜토리얼로 Redirect

--- app/core/github/api/__init__.py
+++ app/core/github/api/__init__.py
@@ -1,6 +1,7 @@
 from uuid import UUID
 
 from fastapi import APIRouter, Request
+from fastapi.responses import RedirectResponse
 
 from app.core.github.logic import act_by_event_type
 from app.common.security.github import check_webhook_valid
@@ -9,6 +10,11 @@ from app.common.security.github import check_webhook_valid
 github_webhook_router = APIRouter(
     prefix='/github'
 )
+
+
+@github_webhook_router.get('/webhook/{bot_id}')
+async def redirect_to_how_to_use(bot_id: UUID, request: Request):
+    return RedirectResponse('https://github.com/kodomomo/GMB/blob/master/README.md')
 
 
 @github_webhook_router.post('/webhook/{bot_id}')
