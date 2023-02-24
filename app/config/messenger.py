1070690318fb31acd53a29f91ae5687508f3da3d
osangu <osangu012@gmail.com>
2023-02-24 13:55:17 +0900

    [DELETE] unused env

--- app/config/messenger.py
+++ app/config/messenger.py
@@ -3,10 +3,4 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-APP_ID = environ['MESSENGER_APP_ID']
-APP_SECRET = environ['MESSENGER_APP_SECRET']
-
 MESSENGER_WEBHOOK_VERIFY_TOKEN = environ['MESSENGER_WEBHOOK_VERIFY_TOKEN']
-REDIRECT_URI = 'https://ac1a-168-188-240-208.jp.ngrok.io/facebook/redierect'
-STATE = environ['STATE']
-PAGE_ID = environ['PAGE_ID']
