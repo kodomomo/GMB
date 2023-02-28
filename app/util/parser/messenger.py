7afb98d8d39c931099f01707cf6bc65dc4783f0e
osangu <osangu012@gmail.com>
2023-02-28 17:32:24 +0900

    [UPDATE] class로 관리

--- app/util/parser/messenger.py
+++ app/util/parser/messenger.py
@@ -2,10 +2,17 @@ from app.core.messenger.api.payload import MessagePayload
 from app.util.random_value import generate_random_secret
 
 
-def payload_to_message(payload: MessagePayload):
-    entry = payload.entry[0]['messaging'][0]
+class Message:
+    def __init__(self, content: str, sender: str):
+        self.content = content
+        self.sender = sender
 
-    return {
-        'secret': message if (message := entry['message'].get('text')) is not None else generate_random_secret(),
-        'sender': entry['sender']['id'],
-    }
+
+def payload_to_message(payload: MessagePayload) -> Message:
+    entry = payload.entry[0]['messaging'][0]
+    message = entry['message'].get('text')
+
+    return Message(
+        message if message is not None else generate_random_secret(),
+        entry['sender']['id']
+    )
