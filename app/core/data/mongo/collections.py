df8643badda5aee8d2f3920aad7a8acbe03c3b61
osangu <osangu012@gmail.com>
2023-05-03 14:45:30 +0900

    [DELETE] origin model

--- app/core/data/mongo/collections.py
+++
@@ -1,8 +0,0 @@
-class CollectionNames:
-    TOKEN = 'messengerToken'
-    WEBHOOK_USER = 'webhookUser'
-    PENDING_WEBHOOK = 'pendingWebhook'
-    WEBHOOK = 'webhook'
-    REPOSITORY = 'repository'
-
-    NAME_LIST = [WEBHOOK, PENDING_WEBHOOK]
