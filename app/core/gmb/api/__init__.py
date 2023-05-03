25e10829304bd6ed4d3af6136e6a9482e541d6ef
osangu <osangu012@gmail.com>
2023-05-03 10:50:46 +0900

    [DELETE] base url

--- app/core/gmb/api/__init__.py
+++ app/core/gmb/api/__init__.py
@@ -4,11 +4,3 @@ from fastapi.responses import RedirectResponse
 gmb_router = APIRouter()
 
 
-@gmb_router.get('/')
-def redirect_base():
-
-    # TODO
-
-    return RedirectResponse(
-        url='https://www.youtube.com/watch?v=DDYJysAYVWo&t=0s'
-    )
