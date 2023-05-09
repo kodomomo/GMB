18414da284c9eeb04542476ba329eed44cb1eb2b
osangu <osangu012@gmail.com>
2023-05-09 09:50:58 +0900

    [DELETE] odm migrated

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 
-from app.core.data.mongo import init_mongo
 from app.core.github import include_github_router
 from app.core.gmb import include_gmb_router
 from app.core.messenger import include_messenger_router
@@ -9,16 +8,9 @@ from app.core.messenger import include_messenger_router
 def create_app():
     app = FastAPI()
 
-    # Mongo
-    init_mongo()
-
     # Router
     include_github_router(app)
     include_messenger_router(app)
     include_gmb_router(app)
 
     return app
-
-# TODO
-# 1. Model Class에서 @Property 변수로 접근 가능하게 해주기
-# 2. query에서 find 함수들 return 값 Decorator를 통해서 Model로 바꿔주기
