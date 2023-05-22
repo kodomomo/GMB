d127c1b4ca0842203fac7df9a53a426d267ac1d9
osangu <osangu012@gmail.com>
2023-05-22 15:07:35 +0900

    [UPDATE] config 파일 이전

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -2,7 +2,23 @@ import os
 
 from dotenv import load_dotenv, find_dotenv
 
-
 load_dotenv(find_dotenv())
 
-BASE_URL = os.environ['BASE_URL']
+
+class GMBConfig:
+    BASE_URL = os.environ['BASE_URL']
+
+
+class MongoConfig:
+    _MONGO_HOST = os.environ['MONGO_HOST']
+    _MONGO_PORT = os.environ['MONGO_PORT']
+    _MONGO_ROOT_NAME = os.environ['MONGO_ROOT_NAME']
+    _MONGO_ROOT_PASSWORD = os.environ['MONGO_ROOT_PASSWORD']
+
+    DB_NAME = os.environ['MONGO_DATABASE_NAME']
+    MONGO_URL = f'mongodb://{_MONGO_ROOT_NAME}:{_MONGO_ROOT_PASSWORD}@{_MONGO_HOST}:{_MONGO_PORT}'
+
+
+class Messenger:
+    PAGE_ACCESS_TOKEN = os.environ['PAGE_ACCESS_TOKEN']
+    MESSENGER_WEBHOOK_VERIFY_TOKEN = os.environ['MESSENGER_WEBHOOK_VERIFY_TOKEN']
--- app/config/messenger.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv, find_dotenv
-
-load_dotenv(find_dotenv())
-
-MESSENGER_WEBHOOK_VERIFY_TOKEN = os.environ['MESSENGER_WEBHOOK_VERIFY_TOKEN']
-PAGE_ACCESS_TOKEN = os.environ['PAGE_ACCESS_TOKEN']
